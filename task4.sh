acc31de56653ea083a670cf615f6ebb684a9f915
TinyTrebuchet <73381089+TinyTrebuchet@users.noreply.github.com>
2021-10-18 01:29:26 +0530

    Add files via upload

---
+++ task4.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [[ -n $1 ]]; then
+	cd $1
+fi
+
+git fetch origin
+
+if [[ -n $(git diff origin/master) ]]; then
+	git merge origin/master
+fi
+
+
+# re-deploy flask
+instance=$(ps aux | grep "/usr/bin/python3 $(pwd)" | head -1 | awk '{print $NF}')
+pid=$(ps aux | grep "/usr/bin/python3 $instance" | head -1 | awk '{print $2}')
+kill -9 $pid
+/usr/bin/python3 $instance
